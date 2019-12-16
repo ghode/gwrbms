970b652f222016f7cbb28f22777de44487c87608
ghode <1257662938@qq.com>
2019-12-16 23:02:05 +0800

    warehouse

---
+++ src/main/java/com/African411/dao/WarehouseMapper.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019. The copyright is reserved by Ghode of Harbin Institute
+ * of Technology. Users are free to copy, change or remove. Because no one
+ * will read this. Only I know is that Repeaters are the best of the world.
+ * Only I know is that Repeaters are the best of the world. Only I know is
+ * that Repeaters are the best of the world. Maybe a long copyright text
+ * seems professional. Therefore this text will be a bit lengthy. However,
+ * the author seems to be afraid that one day, this text may be uploaded to
+ * business projects. That is the time you can contact with author via email
+ * ghode@cirnocraft.im or directly ignore this, which will be interesting.
+ */
+
+package com.African411.dao;
+
+import com.African411.domain.Warehouse;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface WarehouseMapper {
+    int deleteByPrimaryKey(@Param("id") Integer id, @Param("wareId") Integer wareId);
+
+    int insert(Warehouse record);
+
+    Warehouse selectByPrimaryKey(@Param("id") Integer id, @Param("wareId") Integer wareId);
+
+    List<Warehouse> selectAll();
+
+    int updateByPrimaryKey(Warehouse record);
+}
---
+++ src/main/java/com/African411/domain/Warehouse.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019. The copyright is reserved by Ghode of Harbin Institute
+ * of Technology. Users are free to copy, change or remove. Because no one
+ * will read this. Only I know is that Repeaters are the best of the world.
+ * Only I know is that Repeaters are the best of the world. Only I know is
+ * that Repeaters are the best of the world. Maybe a long copyright text
+ * seems professional. Therefore this text will be a bit lengthy. However,
+ * the author seems to be afraid that one day, this text may be uploaded to
+ * business projects. That is the time you can contact with author via email
+ * ghode@cirnocraft.im or directly ignore this, which will be interesting.
+ */
+
+package com.African411.domain;
+
+public class Warehouse {
+    private Integer id;
+
+    private Integer wareId;
+
+    private Long quantity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getWareId() {
+        return wareId;
+    }
+
+    public void setWareId(Integer wareId) {
+        this.wareId = wareId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+}
